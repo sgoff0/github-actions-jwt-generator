2fa98a1f3c37614ca99a44dab14ff62481c7585d
Steven Goff <51921135+sgoff0@users.noreply.github.com>
2020-09-04 15:18:09 -0500

    .

--- dist/index.js
+++ dist/index.js
@@ -5272,11 +5272,13 @@ function run() {
             });
             let jsonOptions = {};
             if (options) {
+                core.info(`Got options: '${options}'`);
                 try {
+                    core.info(`Attempting to parse options`);
                     jsonOptions = JSON.parse(options);
                 }
                 catch (err) {
-                    core.setFailed(`Action failed parsing options ${err}`);
+                    core.setFailed(`Action failed parsing options with error '${err}'`);
                 }
             }
             const token = jwt.sign(payload, secret, jsonOptions);
